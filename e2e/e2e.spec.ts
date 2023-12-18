a97887e2ff55782201bcbba203041be41e23fe76
kanno <812137533@qq.com>
2023-12-18 13:56:26 +0800

    chore: try remix

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -3,23 +3,26 @@ import test from 'ava'
 import { createE2EServer } from './helper'
 import { createSSRServer } from './fixtures/vue-ssr/server'
 
-// FIXME
-// test('fixture remix', async (t) => {
-//   const { page } = await createE2EServer('remix')
-//   await page.waitForSelector('div[role="button"]')
-//   const elementHandle = await page.$('div[role="button"]')
-//   const windowHandle = await page.evaluateHandle(() => Promise.resolve(window))
-//   const red = await page.evaluate(([window, el]) => {
-//     return (window as Window).getComputedStyle(el as Element).color
-//   }, [windowHandle, elementHandle])
-//   t.is(red, 'rgb(255, 0, 0)', 'first load spa button text color should be red')
-//   await elementHandle.click()
-//   await new Promise((resolve) => setTimeout(resolve, 2000))
-//   const blue = await page.evaluate(([window, el]) => {
-//     return (window as Window).getComputedStyle(el as Element).color
-//   }, [windowHandle, elementHandle])
-//   t.is(blue, 'rgb(0, 0, 255)', 'tap button and text color should be blue')
-// })
+// FIXME 
+// I don't know why remix can't inject it self virtual module by node api (But it can work well by cli)
+// I guessing remix doesn't respect root.
+test('fixture remix', async (t) => {
+  const { page } = await createE2EServer('remix')
+  await new Promise((resolve) => setTimeout(resolve, 5000)) 
+  await page.waitForSelector('div[role="button"]')
+  const elementHandle = await page.$('div[role="button"]')
+  const windowHandle = await page.evaluateHandle(() => Promise.resolve(window))
+  const red = await page.evaluate(([window, el]) => {
+    return (window as Window).getComputedStyle(el as Element).color
+  }, [windowHandle, elementHandle])
+  t.is(red, 'rgb(255, 0, 0)', 'first load spa button text color should be red')
+  await elementHandle.click()
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const blue = await page.evaluate(([window, el]) => {
+    return (window as Window).getComputedStyle(el as Element).color
+  }, [windowHandle, elementHandle])
+  t.is(blue, 'rgb(0, 0, 255)', 'tap button and text color should be blue')
+})
 
 test('fixture spa', async (t) => {
   const { page } = await createE2EServer('spa')
