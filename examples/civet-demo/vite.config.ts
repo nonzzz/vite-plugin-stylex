35898fd0b1e2cd202e42e1e560ae27ad4d346c51
kanno <812137533@qq.com>
2024-01-13 12:38:23 +0800

    chore: switch civet-demo react to preact

--- examples/civet-demo/vite.config.ts
+++ examples/civet-demo/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 import { stylexPlugin } from 'vite-plugin-stylex-dev'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat', // Must be below test-utils
+      'react/jsx-runtime': 'preact/jsx-runtime'
+    }
+  },
   // Fixme @stylexjs/stylex don't respect `runtimeInjection` so we should set `dev` as false
-  plugins: [civetPlugin({}), react(), stylexPlugin({ include: /\.(mjs|js|ts|civet|jsx|tsx)(\?.*|)$/, dev: false })]
+  plugins: [civetPlugin({ implicitExtension: false }), react(), stylexPlugin({ include: /\.(mjs|js|ts|civet|jsx|tsx)(\?.*|)$/, dev: false })]
 })
