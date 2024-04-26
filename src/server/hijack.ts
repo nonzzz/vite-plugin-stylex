63630964319b80b08003ef4315a1c89383c3d61f
mengdaoshizhongxinyang <programmercyk@gmail.com>
2024-04-26 14:20:36 +0800

    chore: hijackHook type

--- src/server/hijack.ts
+++ src/server/hijack.ts
@@ -6,9 +6,9 @@ type Next<K extends keyof Plugin> = (
   args: NonNullable<Parameters<HookHandler<Plugin[K]>>>,
 ) => ReturnType<HookHandler<Plugin[K]>>
 
-export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>): undefined
-export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable: false): undefined
-export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable: true): Plugin[K]
+export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>): void
+export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable: false): void
+export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable: true): (...args: Parameters<Next<K>>[2]) => ReturnType<Next<K>>
 export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable = false) {
   if (!plugin[name]) throw new Error(`[vite-plugin-stylex-dev]: ${name} haven't implement yet.`)
   const hook = plugin[name] as any
@@ -17,12 +17,12 @@ export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next
     hook.handler = function (this, ...args: any) {
       return next(fn, this, args)
     }
-    if (executable) return hook as Plugin[K]
+    if (executable) return hook.handler
   } else {
     const fn = hook
     plugin[name] = function (this: any, ...args: any) {
       return next(fn, this, args)
     }
-    if (executable) return plugin[name] as Plugin[K]
+    if (executable) return plugin[name]
   }
 }
