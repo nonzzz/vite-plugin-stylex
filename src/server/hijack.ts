e7bcad5526cccbe81469b6bb26346b1e8b6ce486
mengdaoshizhongxinyang <programmercyk@gmail.com>
2024-04-26 11:20:45 +0800

    chore: hijackHook type

--- src/server/hijack.ts
+++ src/server/hijack.ts
@@ -6,6 +6,9 @@ type Next<K extends keyof Plugin> = (
   args: NonNullable<Parameters<HookHandler<Plugin[K]>>>,
 ) => ReturnType<HookHandler<Plugin[K]>>
 
+export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>): undefined
+export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable: false): undefined
+export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable: true): (this: any, ...args: any[]) => ReturnType<Next<K>>
 export function hijackHook<K extends keyof Plugin>(plugin: Plugin, name: K, next: Next<K>, executable = false) {
   if (!plugin[name]) throw new Error(`[vite-plugin-stylex-dev]: ${name} haven't implement yet.`)
   const hook = plugin[name] as any
